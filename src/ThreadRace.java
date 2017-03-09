3699c0e843a6a28bf8780d616f8fdf0883c2a439
Bill Adams <billadams1977@gmail.com>
2017-03-08 18:57:15 -0600

    submitted for grade

--- src/ThreadRace.java
+++ src/ThreadRace.java
@@ -13,12 +13,19 @@ import java.util.TimerTask;
 import java.awt.event.ActionEvent;
 import java.util.Random;
 
+@SuppressWarnings("serial")
 public class ThreadRace extends JFrame
 {
 
 	private JPanel contentPane;
 	private JLabel lblThreadTwoComplete;
 	private JLabel lblThreadOneComplete;
+	private int int1 = 0;
+	private int int2 = 0;
+	Timer timer1;
+	Timer timer2;
+	TimerTask task1;
+	TimerTask task2;
 
 	/**
 	 * Launch the application.
@@ -45,54 +52,56 @@ public class ThreadRace extends JFrame
 	 * Create the frame.
 	 */
 	public ThreadRace()
-	{
-		Timer timer1 = new Timer();
-		Timer timer2 = new Timer();
-		
+	{	
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 222);
+		setTitle("Multi-threading progressbar race");
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JProgressBar progressBar1 = new JProgressBar();
-		progressBar1.setBounds(139, 49, 146, 14);
+		progressBar1.setBounds(151, 49, 146, 14);
 		contentPane.add(progressBar1);
 		
 		JProgressBar progressBar2 = new JProgressBar();
-		progressBar2.setBounds(139, 88, 146, 14);
+		progressBar2.setBounds(151, 88, 146, 14);
 		contentPane.add(progressBar2);
 		
 		JLabel lblNewLabel = new JLabel("Thread One");
-		lblNewLabel.setBounds(66, 49, 73, 14);
+		lblNewLabel.setBounds(78, 49, 73, 14);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Thread Two");
-		lblNewLabel_1.setBounds(66, 88, 73, 14);
+		lblNewLabel_1.setBounds(78, 88, 73, 14);
 		contentPane.add(lblNewLabel_1);
 		
 		JButton btnStart = new JButton("Start!");
-		btnStart.setBounds(172, 128, 89, 23);
+		
+		btnStart.setBounds(172, 133, 89, 23);
 		btnStart.addActionListener(new ActionListener() 
 		{
 			public void actionPerformed(ActionEvent arg0) 
-			{
-				TimerTask task1 = new TimerTask()
+			{	
+				btnStart.setEnabled(false);
+				
+				// Set up the first task/thread.
+				task1 = new TimerTask()
 				{
-					int num = 0;
 					int randomNum;
 					
 					@Override
 					public void run()
-					{
-						while (num <= progressBar1.getMaximum())
+					{	
+						while (int1 <= (progressBar1.getMaximum() + int1))
 						{	
 							randomNum = randomInteger(0, 10);
 							
 							if (progressBar1.getValue() >= progressBar1.getMaximum())
 							{
 								lblThreadOneComplete.setText("Completed!");
+//								threadOneDone = true;
 							}
 							
 							try
@@ -104,29 +113,30 @@ public class ThreadRace extends JFrame
 								e.printStackTrace();
 							}
 							
-							progressBar1.setValue(num);
-							num += randomNum;
-						}
-						
+							progressBar1.setValue(int1);
+							int1 += randomNum;
+						}	
 					}
 				};
+				timer1 = new Timer();
 				timer1.schedule(task1, 0, 1000);
 				
-				TimerTask task2 = new TimerTask()
+				// Set up the second task/thread.
+				task2 = new TimerTask()
 				{
-					int num = 0;
 					int randomNum;
 					
 					@Override
 					public void run()
 					{
-						while (num <= progressBar2.getMaximum())
+						while (int2 <= (progressBar2.getMaximum() + int2))
 						{
 							randomNum = randomInteger(0, 10);
 							
 							if (progressBar2.getValue() >= progressBar2.getMaximum())
 							{
 								lblThreadTwoComplete.setText("Completed!");
+//								threadTwoDone = true;
 							}
 							
 							try
@@ -138,12 +148,12 @@ public class ThreadRace extends JFrame
 								e.printStackTrace();
 							}
 							
-							progressBar2.setValue(num);
-							num += 4;
-						}
-						
+							progressBar2.setValue(int2);
+							int2 += 4;
+						}					
 					}
 				};
+				timer2 = new Timer();
 				timer2.schedule(task2, 0, 1000);
 			}
 			
@@ -157,6 +167,10 @@ public class ThreadRace extends JFrame
 		lblThreadTwoComplete = new JLabel("");
 		lblThreadTwoComplete.setBounds(301, 88, 79, 14);
 		contentPane.add(lblThreadTwoComplete);
+		
+		JLabel lblRestartTheProgram = new JLabel("Restart the program to run another thread race.");
+		lblRestartTheProgram.setBounds(78, 11, 277, 14);
+		contentPane.add(lblRestartTheProgram);
 	}
 	
 	public static int randomInteger(int min, int max)
